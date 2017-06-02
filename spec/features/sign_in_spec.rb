086a54ff52972a6c90ee5469cef117ab1236bfc0
Heather Doucet <heatherredoucet@gmail.com>
2017-06-02 11:12:21 -0500

    add signin authentication

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -6,9 +6,41 @@ describe "Signing In" do
 
     click_link 'Sign In'
 
-    expect(current_path).to eq(new_session_path)
+    expect(current_path).to eq(signin_path)
 
     expect(page).to have_field("Email")
     expect(page).to have_field("Password")
+  end
+
+  it "signs in user with valid email/password combination" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button 'Sign In'
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "displays error message when email/password combination are invalid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link 'Sign In'
+
+    fill_in "Email", with: user.email
+    fill_in "Password", with: "no match"
+
+    click_button 'Sign In'
+
+    expect(page).to have_text('Invalid')
   end
 end
