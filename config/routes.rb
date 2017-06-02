405d0dcc9f1e09c243f2590ec37b7059a1b748ef
Heather Doucet <heatherredoucet@gmail.com>
2017-06-02 10:35:33 -0500

    adding sessions for user signin/signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  resource :session
+
   resources :users
   root "movies#index"
   get 'signup' => 'users#new'
+  get 'signin' => 'sessions#new'
   resources :movies do
     resources :reviews
   end
